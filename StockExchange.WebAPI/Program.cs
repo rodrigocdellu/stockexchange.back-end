8295b78f8cd59a911383c15c8c379bd4edcfd253
Rodrigo C. Dellu <rodrigocdellu.trabalho@outlook.com>
2025-04-20 20:09:06 -0300

    Now Swagger will run in the production environment

--- StockExchange.WebAPI/Program.cs
+++ StockExchange.WebAPI/Program.cs
@@ -1,4 +1,5 @@
 // 2025/02/20 - Required for Dependency Injection (IoC)
+using Microsoft.OpenApi.Models;
 using StockExchange.WebAPI.Service;
 
 // 2025/02/20 - Define the policy name
@@ -8,9 +9,16 @@ const string POLICYFORCORS = "StockExchangePolicy";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container. Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddControllers(); // 2025/02/20 - Add controllers for automatic mapping
-builder.Services.AddSwaggerGen();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => { // 2025/02/20 - For Swagger to run in the production environment
+    options.SwaggerDoc("v1", new OpenApiInfo{
+        Version = "v1",
+        Title = "StockExchange.WebAPI",
+        Description = "For StockExchange usage",
+        TermsOfService = new Uri("https://github.com/rodrigocdellu")
+    });
+});
 builder.Services.AddTransient<IContentService, ContentService>(); // 2025/02/20 - Add the Dependency Injection (IoC)
 
 // 2025/02/20 - Add CORS with a policy
@@ -27,13 +35,17 @@ builder.Services.AddCors(options =>
 // Build the application
 var app = builder.Build();
 
+/* ToDo: 2025/02/20 - Remove this later.
 // Configure the HTTP request pipeline
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+*/
 
+app.UseSwagger(); // 2025/02/20 - For Swagger to run in the production environment
+app.UseSwaggerUI(); // 2025/02/20 - For Swagger to run in the production environment
 app.UseCors(POLICYFORCORS); // 2025/02/20 - Use CORS with the policy created
 app.MapControllers(); // 2025/02/20 - Map the application controllers
 
