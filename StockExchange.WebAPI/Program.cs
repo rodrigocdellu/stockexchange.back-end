7f9969a206997e48fcee7bb49462e01df1d43fa0
Rodrigo C. Dellu <rodrigocdellu.trabalho@outlook.com>
2025-04-23 16:09:06 -0300

    SonarQube - Refactor your code not to use hardcoded absolute paths or URIs.

--- StockExchange.WebAPI/Program.cs
+++ StockExchange.WebAPI/Program.cs
@@ -2,8 +2,8 @@
 using Microsoft.OpenApi.Models;
 using StockExchange.WebAPI.Services;
 
-// 2025/04/20 - Define the policy name
-const string POLICYFORCORS = "StockExchangePolicy";
+const string POLICYFORCORS = "StockExchangePolicy"; // 2025/04/20 - Define the policy name
+const string TERMSOFSERVICEURI = "https://github.com/rodrigocdellu"; // 2025/04/23 - SonarQube - Refactor your code not to use hardcoded absolute paths or URIs
 
 // Create the application builder
 var builder = WebApplication.CreateBuilder(args);
@@ -16,7 +16,7 @@ builder.Services.AddSwaggerGen(options => { // 2025/04/20 - For Swagger to run i
         Version = "v1",
         Title = "StockExchange.WebAPI",
         Description = "For StockExchange usage",
-        TermsOfService = new Uri("https://github.com/rodrigocdellu")
+        TermsOfService = new Uri(TERMSOFSERVICEURI)
     });
 });
 builder.Services.AddSingleton<IApplicationService, ApplicationService>(); // 2025/04/22 - To deal with application information
